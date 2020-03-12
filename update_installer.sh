1b1c413abedfd722d8f818f2702a94f2311acfb7
Johannes Blaschke <jpblaschke@lbl.gov>
2020-03-12 10:58:06 -0700

    make installers executable

--- update_installer.sh
+++ update_installer.sh
@@ -3,7 +3,9 @@
 
 rm Miniconda3-latest-Linux-ppc64le.sh
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-ppc64le.sh
+chmod u+x Miniconda3-latest-Linux-ppc64le.sh
 
 
 rm Miniconda3-latest-Linux-x86_64.sh  
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh 
+chmod u+x Miniconda3-latest-Linux-x86_64.sh
