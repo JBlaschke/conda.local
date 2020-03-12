a77695e30debc9b02967f16b172bb80fb4f6be60
Johannes Blaschke <jpblaschke@lbl.gov>
2020-03-12 10:23:56 -0700

    added power pc installer

--- update_installer.sh
+++ update_installer.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
+
+rm Miniconda3-latest-Linux-ppc64le.sh
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-ppc64le.sh
+
+
 rm Miniconda3-latest-Linux-x86_64.sh  
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh 
